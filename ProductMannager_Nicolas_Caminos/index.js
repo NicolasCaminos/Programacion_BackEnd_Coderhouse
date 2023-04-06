3968d441589a9f4a2b4a4aa4344449ad2552d444
Nicolás Maximiliano Caminos <45422512+NicolasCaminos@users.noreply.github.com>
2023-04-06 19:17:39 -0300

    Add files via upload

---
+++ ProductMannager_Nicolas_Caminos/index.js
@@ -0,0 +1,30 @@
+import ProductManager from "./managers/ProductManager.js";
+
+const productManager = new ProductManager();
+
+const context = async () => {
+    const test = await productManager.getProducts();
+    console.log(test);
+    let productTest = {
+        title: 'producto prueba',
+        description: 'Este es un producto prueba',
+        price: 200,
+        thumbnail: 'Sin imagen',
+        code: 'abc123',
+        stock: 25
+    }
+
+    await productManager.addProducts(productTest);
+
+    const newUsers = await productManager.getProducts();
+    console.log(newUsers);
+
+    const busquedaPorId = await productManager.getProductById(1);
+    console.log(busquedaPorId)
+
+    await productManager.deleteProduct(1);
+    await productManager.updateProduct(1, 'title', 'producto prueba de Test')
+    console.log(await productManager.getProductById(1));
+}
+
+context();
---
+++ ProductMannager_Nicolas_Caminos/managers/ProductManager.js
@@ -0,0 +1,75 @@
+import fs from 'fs';
+
+
+export default class ProductManager {
+    constructor() {
+        this.path = './files/Product.json';
+    }
+
+    getProducts = async () => {
+        if (fs.existsSync(this.path)) {
+            const data = await fs.promises.readFile(this.path, 'utf-8');
+            const products = JSON.parse(data);
+            return products;
+        }
+        return [];
+    }
+
+    addProducts = async (product) => {
+        const products = await this.getProducts();
+        if (products.length == 0) {
+            product.id = 1;
+        } else {
+            product.id = products[products.length - 1].id + 1;
+        }
+        products.push(product);
+        await fs.promises.writeFile(this.path, JSON.stringify(products, null, '\t'));
+        return products
+    }
+    getProductById = async (searchById) => {
+        const data = await fs.promises.readFile(this.path, 'utf-8');
+        const products = JSON.parse(data);
+        const Existe = products.find(p => p.id === searchById);
+        if (Existe) {
+            return products;
+        } else {
+            console.log("No existe un producto con el ID");
+        }
+    }
+
+    deleteProduct = async (_id) => {
+        const data = await fs.promises.readFile(this.path, 'utf-8');
+        const products = JSON.parse(data);
+        const Existe = products.find(products => products.id === _id);
+        console.log("***********************************");
+        if (!Existe) {
+            console.log("No Existe un producto con el ID");
+        }
+        const deleteProduct = products.filter(products => products.id !== _id);
+        console.log(deleteProduct)
+    }
+
+    updateProduct = async (_id, atribute, value) => {
+
+        const datas = await fs.promises.readFile(this.path, 'utf-8');
+        const productos = JSON.parse(datas);
+
+        const item_index = productos.findIndex(p => p.id === _id)
+
+        if (item_index < 0) {
+            console.info(`no product with the next id: ${_id}`)
+            return null
+        }
+
+        // updates item by index, and changes the attribute with the value
+        // e.g 
+        // await manager.updateProduct(1, 'nombre', 'Cacharro, ups')
+
+        const selectedItem = productos[item_index];
+        selectedItem[atribute] = value;
+        productos[item_index] = selectedItem;
+
+        const cartJson = JSON.stringify(productos)
+        await fs.promises.writeFile(this.path, cartJson);
+    }
+}
