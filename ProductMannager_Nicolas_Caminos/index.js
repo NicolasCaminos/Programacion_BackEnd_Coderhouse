ce65cbbca8841698ce6e3b151bd69e2911dd5395
Nicolás Maximiliano Caminos <nicoasis7@gmail.com>
2023-04-06 21:22:53 -0300

    0.1

--- ProductMannager_Nicolas_Caminos/index.js
+++ ProductMannager_Nicolas_Caminos/index.js
@@ -14,7 +14,7 @@ const context = async () => {
         stock: 25
     }
 
-    let productTestUpdate = {
+    let productTest = {
         title: 'producto prueba',
         description: 'Este es un producto prueba',
         price: 200,
@@ -23,16 +23,16 @@ const context = async () => {
         stock: 25
     }
 
-    //await productManager.addProducts(productTest);
+    await productManager.addProducts(productTest);
 
-    //const newUsers = await productManager.getProducts();
-    //console.log(newUsers);
+    const newUsers = await productManager.getProducts();
+    console.log(newUsers);
 
-    //const busquedaPorId = await productManager.getProductById(1);
-    //console.log(busquedaPorId)
+    const busquedaPorId = await productManager.getProductById(1);
+    console.log(busquedaPorId)
 
-    // await productManager.deleteProduct(1);
-    await productManager.updateProduct(1, productTestUpdate)
+    await productManager.deleteProduct(1);
+    await productManager.updateProduct(1, 'title', 'producto prueba de Test')
     console.log(await productManager.getProductById(1));
 }
 
